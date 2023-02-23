81a32c6aa776153c0818d567c44fe5e10a185c6d
Toomas Erik Anijärv <toomaserikanijarv@gmail.com>
2023-02-24 09:16:17 +1000

    Edit requirements

--- setup.py
+++ setup.py
@@ -22,16 +22,16 @@ test_requirements = ['pytest>=3']
 setup(
     author="Toomas Erik Anijärv",
     author_email='toomaserikanijarv@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="HLR - Hierarchical Linear Regression for Python",
     entry_points={
